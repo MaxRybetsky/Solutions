c5737bbdead3edb1db79ba6f14bf905c26b3f0f8
MaxRybetsky <stsdewar@ya.ru>
2021-02-02 23:13:17 +0300

    Solution of https://www.codewars.com/kata/597c684822bc9388f600010f/train/java

---
+++ src/test/java/code/get_full_name/DinglemouseTest.java
@@ -0,0 +1,12 @@
+package code.get_full_name;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DinglemouseTest {
+    @Test
+    public void test() {
+        assertEquals("Clint Eastwood", new Dinglemouse("Clint", "Eastwood").getFullName());
+    }
+}
