76dd7480234d9667a99a4504b1beabfdc66d4254
MaxRybetsky <stsdewar@ya.ru>
2021-02-14 22:46:44 +0300

    Solution for https://www.codewars.com/kata/5b45e4b3f41dd36bf9000090/java

---
+++ src/test/java/code/guessingSequence/JomoPipiTest.java
@@ -0,0 +1,26 @@
+package code.guessingSequence;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class JomoPipiTest {
+    @Test
+    public void test0() {
+        int[] result = {1, 10, 11, 12, 13, 14, 15, 16, 2, 3, 4, 5, 6, 7, 8, 9};
+        int x = 16;
+        String expected = Arrays.toString(result),
+                actual = Arrays.toString(JomoPipi.sequence(x));
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void test1() {
+        int[] result = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int x = 9;
+        String expected = Arrays.toString(result),
+                actual = Arrays.toString(JomoPipi.sequence(x));
+        assertEquals(expected, actual);
+    }
+}
