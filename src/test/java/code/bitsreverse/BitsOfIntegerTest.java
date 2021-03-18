d87c6096618584168d22b82742226e09bf629a28
MaxRybetsky <stsdewar@ya.ru>
2021-03-18 23:04:11 +0300

    Solution for https://www.codewars.com/kata/5959ec605595565f5c00002b

---
+++ src/test/java/code/bitsreverse/BitsOfIntegerTest.java
@@ -0,0 +1,17 @@
+package code.bitsreverse;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BitsOfIntegerTest {
+    @Test
+    public void fixed_tests() {
+        assertEquals(BitsOfInteger.reverse_bits(417), 267);
+        assertEquals(BitsOfInteger.reverse_bits(267), 417);
+        assertEquals(BitsOfInteger.reverse_bits(0), 0);
+        assertEquals(BitsOfInteger.reverse_bits(2017), 1087);
+        assertEquals(BitsOfInteger.reverse_bits(1023), 1023);
+        assertEquals(BitsOfInteger.reverse_bits(1024), 1);
+    }
+}
